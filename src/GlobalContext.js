6b553e6c49690f10f57f3b4cba2bf5fb68ab0456
Adrian Berenguel <adrianberenguel90@gmail.com>
2023-08-10 23:08:06 +0800

    Created globalcontext object and globalstate component

---
+++ src/GlobalContext.js
@@ -0,0 +1,89 @@
+import { createContext, useState } from "react";
+
+export const GlobalContext = createContext({
+  searchParam: '', // value of the search input
+  handleOnChange: () => {}, // method
+  handleSubmit: () => {}, // method
+  movieList: [], // to store the movies that we are getting from an API
+  loading: false // this is a loading state to show a message while the API request is processing
+});
+
+const GlobalState = ({children}) => { // a React component that uses the Context API to manage global state.
+  const [searchParam, setSearchParam] = useState('');
+  const handleOnChange = (event) => {
+    console.log(event.target.value);
+    setSearchParam(event.target.value);
+  }
+  const handleSubmit = () => {
+    
+  }
+  const contextValue = {
+    searchParam, // value of the search input
+    handleOnChange, // method
+    handleSubmit, // method
+    movieList: [], // to store the movies that we are getting from an API
+    loading: false // this is a loading state to show a message while the API request is processing
+  }
+
+  return ( // parethesis is not needed if return statement is only one line
+    <GlobalContext.Provider value={contextValue}>
+      {children}
+    </GlobalContext.Provider>
+  )
+};
+
+export default GlobalState;
+
+/**
+ * * When do you need context/useContext?
+ *  - The main idea of using the context is to allow your components to access global data and re-render when that
+ *    global data is changed. Context solves the props drilling problem: when you have to pass down props from
+ *    parents to children.
+ *  - Context has atleast 2 properties Consumer and Provider
+ *    - Consumer - The Consumer component allows a component to subscribe to context changes.
+ *      The consumer components allow to use data from the context. 
+ *    - Provider - This will basically provide value to your components and subcomponents.
+ * 
+ * * GlobalContext Code flow:
+ *  - Info: This code exports a context object called GlobalContext.
+ *    - This context object has five properties: searchParam, handleOnChange, handleSubmit, movieList and loading
+ *    - This context object can be used to share data and state between different components in our application.
+ *      For example, we could use it to share the search input value between the search input component and
+ *      the movie list component.
+ *    - To use the GlobalContext context object, we need to wrap our application with a Provider component.
+ *      The Provider component takes the context object as its value prop.
+ *  - The properties inside GlobalContext variable are the things we need
+ *    - This variable is also being exported for the proper file to use
+ *  - The things we need for this file are the value of the search input and handleOnChange method of the input
+ * 
+ * * GlobalState Code flow:
+ *  - Info: GlobalState is a React component that uses the Context API to manage global state
+ *    - This component defines the following state: searchParam, handleOnChange, handleSubmit, movieList, movieList
+ *    - This component also defines a contextValue object that contains all of the state values.
+ *      This object is then passed to the GlobalContext.Provider component, which makes the state values available
+ *      to all of the child components.
+ *    - The GlobalState component can be used to share data between multiple components in a React application.
+ *      For example, we have a search bar and a list of movies, we can use the GlobalState component to share the
+ *      search parameter between the two components. This way, the list of movies will be updated automatically
+ *      whenever the user changes the search parameter.
+ * 
+ *  - The variable GlobalState will return <GlobalContext.Provider></GlobalContext.Provider>
+ *    - {children} is basically all the components being passed in the <GlobalContext.Provider></GlobalContext.Provider>
+ *    - This variable is also being exported for the proper file to use
+ *  - We need to pass the value in the GlobalState through the contextValue variable object
+ *    - For now it contains the properties of GlobalContext since we have no state yet
+ *  - Create a state
+ *    - The code snippet const [searchParam, setSearchParam] = useState(''); is using the React useState hook to create a state
+ *      variable called searchParam. The initial value of the state variable is an empty string (''). The setSearchParam
+ *      function is used to update the value of the state variable.
+ *    - The searchParam variable is passed inside the contextValue variable
+ *  - handleOnChange() method is created
+ *  - handleSubmit() method is created
+ *  - contextValue is being passed as a value in <GlobalContext.Provider value={contextValue}></GlobalContext.Provider>
+ *  - We need to grab this provider in our root component
+ *    - Wrap the elements in index.js file with <GlobalState></<GlobalState>
+ *  - The value that the user types in the search input will be saved in searchParam variables
+ *    - Any change events in the input is handled by handleOnChange function
+ *  - Read and search "We need to save the value that the user searches in the searchParam variable" in Search.jsx
+ * 
+ */
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../GlobalContext";
 
 const Search = () => {
+  const { searchParam, handleOnChange, handleSubmit } = useContext(GlobalContext);
+
   return (
     <div>
-      <input name="search" placeholder="Enter movie keyword here" />
-      <button>Submit search</button>
+      <input
+        name="search"
+        // value={searchParam}
+        onChange={handleOnChange}
+        placeholder="Enter movie keyword here"
+      />
+      <button onClick={handleSubmit}>Submit search</button>
     </div>
-  )
+  );
 };
 
 export default Search;
+
+/**
+ * * We need to save the value that the user searches in the searchParam variable
+ *  - Info: Inside any component that needs to access the GlobalContext context object, we can use the useContext() hook.
+ *    The useContext() hook takes the context object as its argument and returns the current value of the context object.
+ *    For example: <input value={searchParam} onChange={handleOnChange}/>
+ *    - The useContext() hook will automatically update the value of the context object when it changes.
+ *      This means that we don't need to worry about manually updating the value of the context object in our components.
+ * 
+ *  - To do that, we need to import the variables and methods from GlobalContext file
+ *    - We destructure the contents of GlobalContext through
+ *      const { searchParam, handleOnChange, handleSubmit } = useContext(GlobalContext);
+ *  - Pass the value of the input through the searchParam as well as the handleOnChange function. 
+ *    Example: <input value={searchParam} onChange={handleOnChange} />
+ *  - Button click event is handled by handleSubmit
+ * 
+ */
